28c1ec32d00d45ccb964fdf584373af4d95a5da6
ranyigit <ranyi_num@163.com>
2014-11-06 10:39:20 +0800

    新基础数据库

--- doc/myhuai-沪爱初始化数据库基础数据.sql
+++ doc/myhuai-沪爱初始化数据库基础数据.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : local
 Source Server Version : 50615
 Source Host           : localhost:3306
-Source Database       : wxtest
+Source Database       : myhuai
 
 Target Server Type    : MYSQL
 Target Server Version : 50615
