74f0b9d13e7187804463d697e188301b1af32701
ranyigit <ranyi_num@163.com>
2014-11-22 17:34:02 +0800

    自助订单继续完善

--- src/Lanrain/Lib/Action/User/Self_orderAction.class.php
+++ src/Lanrain/Lib/Action/User/Self_orderAction.class.php
@@ -12,5 +12,37 @@ class Self_orderAction extends UserAction{
 		$this->display();
 	}	
 	
+	/**
+	 * 获取商品分类及分类下服务和资讯树杈结构
+	 * @return json
+	 */
+	public function get_category(){
+		$where["parentid"]= 0;
+		$res = $this->_mod->where($where)->order("level ASC")->select();
+		foreach ($res as $key => $val){
+			$res1 = $this->_mod->where(array('parentid'=>$val['id']))->order("level ASC")->select();
+			$res[$key]['c'] = $res1;
+			foreach($res1 as $keys => $va){
+				$res2 = $this->_mod->where(array('parentid'=>$va['id']))->order("level ASC")->select();
+				if(!empty($res2) && $res){
+					$res[$key]['c'][$keys]['d'] = $res2;
+					foreach($res2 as $keyT => $vaT){
+						$resService = M('service')->where(array("cate_id"=>$vaT['id']))->select();
+						$resArticle = M('article_new')->where(array("cate_id"=>$vaT['id']))->select();
+						$res[$key]['c'][$keys]['d'][$keyT]['e'][]= $resService;
+						$res[$key]['c'][$keys]['d'][$keyT]['e'][]= $resArticle;
+					}
+				}else{
+			
+				}
+			}
+		}
+		dump($res);
+		//echo json_encode($res);
+	}
+	
+	public function test(){
+		$this->get_category();
+	}
 }
 ?>
