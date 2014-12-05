befa6041cf77e3bf8092b36e5552484124ed0c74
chuiqingdeyi <1119361564@qq.com>
2014-12-05 13:47:02 +0800

    为我推荐样式微调

--- src/weTall/static/weixin/js/home/top.js
+++ src/weTall/static/weixin/js/home/top.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
     $(function () {
     $(window).scroll(function(){
-    if ($(window).scrollTop()>200){
+    if ($(window).scrollTop()>100){
     $("#back-to-top").fadeIn(500);
     }
     else
