d85a4e0e7dde0df627edfb033bc7cd3d6c03fc56
chuiqingdeyi <1119361564@qq.com>
2014-12-06 15:52:11 +0800

    样式微调

--- src/weTall/static/weixin/js/home/top.js
+++ src/weTall/static/weixin/js/home/top.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     });
     });
 
-$(document).ready(function(){
+/*$(document).ready(function(){
     //首先将#back-to-top隐藏
     
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
@@ -36,7 +36,7 @@ $(document).ready(function(){
     });
    
     });
-    });
+    });*/
 
 /*function touchMoveFunc(evt) {
     try {
