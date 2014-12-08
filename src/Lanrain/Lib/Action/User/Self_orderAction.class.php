6209ccf25c39e3c7b7f47b6526f172996cdccc44
ranyigit <ranyi_num@163.com>
2014-12-08 15:58:50 +0800

    测试

--- src/Lanrain/Lib/Action/User/Self_orderAction.class.php
+++ src/Lanrain/Lib/Action/User/Self_orderAction.class.php
@@ -67,6 +67,7 @@ class Self_orderAction extends UserAction{
     	$keywords = $this->_post('keywords','trim');
     	$where['name'] = array('like','%'.$keywords.'%');
     	$resService = M('service')->where($where)->select();
+    	dump($resService);die;
     	if($resService == null || empty($resService)){
     		$resService = $arr;
     	}else{
