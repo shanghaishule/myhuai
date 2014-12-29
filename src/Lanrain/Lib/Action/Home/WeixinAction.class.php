3806e497c43d42112a35184cdf8d0d24a77c171b
ranyigit <ranyi_num@163.com>
2014-12-29 16:36:16 +0800

    关注问题

--- src/Lanrain/Lib/Action/Home/WeixinAction.class.php
+++ src/Lanrain/Lib/Action/Home/WeixinAction.class.php
@@ -783,7 +783,7 @@ class WeixinAction extends Action
             );
         } else {
             if ($home['apiurl'] == false) { 
-            	$url = rtrim(C('site_url'), '/') . 'weTall/index.php?g=home&m=index&a=index&token='. $this->token .'&wecha_id='.$this->data['FromUserName'];
+            	$url = rtrim(C('site_url'), '/') . '/weTall/index.php?g=home&m=index&a=index&token='. $this->token .'&wecha_id='.$this->data['FromUserName'];
             } else {
                 $url = $home['apiurl'].'&wecha_id='.$this->data['FromUserName'];
             }
