0210fbaedc8f5c03880868f97c7e97abcfa3a406
chuiqingdeyi <1119361564@qq.com>
2015-07-27 17:40:09 +0800

    NEW

--- src/weTall/app/Lib/Action/home/indexAction.class.php
+++ src/weTall/app/Lib/Action/home/indexAction.class.php
@@ -1,5 +1,5 @@
 <?php
-class indexAction  {//extends frontendAction
+class indexAction extends baseAction {//extends frontendAction
     public function _initialize(){
     	parent::_initialize();
     	$this->_cat = M('category');
